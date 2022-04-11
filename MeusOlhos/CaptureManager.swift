ca83115bea94783e63066fcea522041eec28129f
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-04-10 22:10:55 -0300

    Device configuration
    Configurando o dispositivo que será utilizado junto com as permissões de acesso.

--- MeusOlhos/CaptureManager.swift
+++ MeusOlhos/CaptureManager.swift
@@ -20,5 +20,28 @@ class CaptureManager {
     init() {
         
     }
-    func startCameraCapture() -> 
+    func startCameraCapture() -> AVCaptureVideoPreviewLayer? {
+        if askForPermission() {
+            guard let captureDevice = AVCaptureDevice.default(for: .video) else {return nil}
+            
+        }
+    }
+    
+    func askForPermission() -> Bool {
+        var hasPermission: Bool = true
+        
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            hasPermission = true
+        case . notDetermined:
+            AVCaptureDevice.requestAccess(for: .video, completionHandler: { (sucess) in
+                hasPermission = sucess
+            })
+        case .restricted, .denied:
+            hasPermission = false
+        }
+        
+        return hasPermission
+   
+    }
 }
